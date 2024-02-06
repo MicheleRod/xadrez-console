b5aa50c899f9f44e1a6ace7a99d620925ae9673d
Michele Rodrigues <chelynha2009@gmail.com>
2024-02-06 10:00:45 -0300

    Acrescentando as demais pecas

---
+++ xadrez-console/xadrez/Peao.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class Peao : Peca
+    {
+        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+        private bool existeInimigo(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null || p.cor != cor;
+        }
+
+        private bool livre(Posicao pos)
+        {
+            return tab.peca(pos) == null;
+        }
+
+        public override bool[,] movimentosPossíveis()
+        {
+            //Peao branco só move p cima, se preto move para baixo
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            if (cor == Cor.Branca)
+            {
+                pos.definirValores(posicao.linha - 1, posicao.coluna); //pode andar p cima se a casa estiver livre
+                if (tab.posicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha - 2, posicao.coluna); //pode andar 2 casas acima se estiver livre e se for o 1ºmovimento do peao
+                if (tab.posicaoValida(pos) && livre(pos) && qtdeMovimentos == 0)
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha - 1, posicao.coluna - 1); //pode andar nas diagonais se existir inimigo na diagonal
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha - 1, posicao.coluna + 1); //pode andar nas diagonais se existir inimigo na diagonal
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+            else {
+                pos.definirValores(posicao.linha + 1, posicao.coluna); //pode andar p cima se a casa estiver livre
+                if (tab.posicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 2, posicao.coluna); //pode andar 2 casas acima se estiver livre e se for o 1ºmovimento do peao
+                if (tab.posicaoValida(pos) && livre(pos) && qtdeMovimentos == 0)
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 1, posicao.coluna - 1); //pode andar nas diagonais se existir inimigo na diagonal
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 1, posicao.coluna + 1); //pode andar nas diagonais se existir inimigo na diagonal
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+            return mat;
+        }
+        
+    }
+}
